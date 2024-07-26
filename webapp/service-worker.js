f9c42a893340477cc0eb986de5a0b9fb43906954
Viacheslav Rutkovskiy <viachaslav.rutkovskiy@gmail.com>
2024-07-26 12:31:54 +0000

    more o less working

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -33,42 +33,69 @@ self.addEventListener('activate', function (event) {
 	);
 });
 
+self.addEventListener('push', (e) => {
+	const body = e.data.text() || 'Push message has no payload';
+  
+	const options = {
+	  body,
+	//   icon: 'images/checkmark.png',
+	  vibrate: [100, 50, 100],
+	  data: {
+		dateOfArrival: Date.now(),
+		primaryKey: 1,
+	  },
+	//   actions: [
+	// 	{
+	// 	  action: 'explore',
+	// 	  title: 'Explore this new world',
+	// 	//   icon: 'images/checkmark.png',
+	// 	},
+	// 	{
+	// 	  action: 'close',
+	// 	  title: "I don't want any of this",
+	// 	//   icon: 'images/xmark.png',
+	// 	},
+	//   ],
+	};
+	e.waitUntil(self.registration.showNotification('Notification via Server and Push API', options));
+  });
+
 // During runtime, get files from cache or -> fetch, then save to cache
-self.addEventListener('fetch', function (event) {
-	// only process GET requests
-	if (event.request.method === 'GET') {
-		event.respondWith(
-			caches.match(event.request).then(function (response) {
-				if (response) {
-					return response; // There is a cached version of the resource already
-				}
+// self.addEventListener('fetch', function (event) {
+// 	// only process GET requests
+// 	if (event.request.method === 'GET') {
+// 		event.respondWith(
+// 			caches.match(event.request).then(function (response) {
+// 				if (response) {
+// 					return response; // There is a cached version of the resource already
+// 				}
 	
-				let requestCopy = event.request.clone();
-				return fetch(requestCopy).then(function (response) {
-					// opaque responses cannot be examined, they will just error
-					if (response.type === 'opaque') {
-						// don't cache opaque response, you cannot validate it's status/success
-						return response;
-					// response.ok => response.status == 2xx ? true : false;
-					} else if (!response.ok) {
-						console.error(response.statusText);
-					} else {
-						return caches.open(CACHE_NAME).then(function(cache) {
-							cache.put(event.request, response.clone());
-							return response;
-						// if the response fails to cache, catch the error
-						}).catch(function(error) {
-							console.error(error);
-							return error;
-						});
-					}
-				}).catch(function(error) {
-					// fetch will fail if server cannot be reached,
-					// this means that either the client or server is offline
-					console.error(error);
-					return caches.match('offline-404.html');
-				});
-			})
-		);
-	}
-});
+// 				let requestCopy = event.request.clone();
+// 				return fetch(requestCopy).then(function (response) {
+// 					// opaque responses cannot be examined, they will just error
+// 					if (response.type === 'opaque') {
+// 						// don't cache opaque response, you cannot validate it's status/success
+// 						return response;
+// 					// response.ok => response.status == 2xx ? true : false;
+// 					} else if (!response.ok) {
+// 						console.error(response.statusText);
+// 					} else {
+// 						return caches.open(CACHE_NAME).then(function(cache) {
+// 							cache.put(event.request, response.clone());
+// 							return response;
+// 						// if the response fails to cache, catch the error
+// 						}).catch(function(error) {
+// 							console.error(error);
+// 							return error;
+// 						});
+// 					}
+// 				}).catch(function(error) {
+// 					// fetch will fail if server cannot be reached,
+// 					// this means that either the client or server is offline
+// 					console.error(error);
+// 					return caches.match('offline-404.html');
+// 				});
+// 			})
+// 		);
+// 	}
+// });
